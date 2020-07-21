8827fde4d94dfe5fcc370a488513d0f152cdbc0a
Bruno Martinez <bruno.h_m@hotmail.com>
2020-07-20 22:53:22 -0300

    feat(CepApiController): Adiciona endpoint de Cadastrar

--- Aula02AspNet/Controllers/CepApiController.cs
+++ Aula02AspNet/Controllers/CepApiController.cs
@@ -19,12 +19,21 @@ namespace Aula02AspNet.Controllers
             _viaCepDAO = viaCepDAO;
         }
 
-        // Get: /api/Endereco/ListarEderecos 
+        // Get: /api/Endereco/ListarEnderecos
         [HttpGet]
         [Route ("ListarEnderecos")]
         public List<ViaCep> Listar()
         {
             return _viaCepDAO.Listar(); 
         }
+
+        // Post: /api/Endereco/CadastrarEndereco
+        [HttpPost]
+        [Route("CadastrarEndereco")]
+        public void CadastrarEnderecos (ViaCep cep)
+        {
+             _viaCepDAO.Cadastrar(cep);
+        }
+
     }
 }
